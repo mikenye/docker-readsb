498b060076a385577b4ff8b68978055568c7193c
mikenye <mike.nye@xpanse.com.au>
2020-02-18 16:43:31 +0800

    Fix typo

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ if [ -f "Dockerfile.${ARCH}" ]; then
     # Build
     echo Building from Dockerfile.${ARCH}
     docker build -f Dockerfile.${ARCH} -t ${IMAGE}:development-${ARCH} . && \
-    VERSION=`docker run --rm -it --entrypoint /usr/local/bin/readsb mikenye/readsb:development-amd64 -V | tr -s " " | tr -d "\r" | tr -d "\n" | cut -d " " -f 2` && \
+    VERSION=`docker run --rm -it --entrypoint /usr/local/bin/readsb mikenye/readsb:development-${ARCH} -V | tr -s " " | tr -d "\r" | tr -d "\n" | cut -d " " -f 2` && \
     echo Tagging as ${IMAGE}:${VERSION}-${ARCH} && \
     docker tag ${IMAGE}:development-${ARCH} ${IMAGE}:${VERSION}-${ARCH}
 else
