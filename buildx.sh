528304d877ad958c38513bd7e35bc32e1e9f6a42
mikenye <mike.nye@xpanse.com.au>
2020-03-20 17:13:17 +0800

    Linting & tidy-up

--- buildx.sh
+++ buildx.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env sh
+#shellcheck shell=sh
 
 REPO=mikenye
 IMAGE=readsb
+PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64"
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
 
 # Build & push latest
-docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+docker buildx build -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
 
 # Get piaware version from latest
-docker pull ${REPO}/${IMAGE}:latest
-VERSION=$(docker run --rm --entrypoint cat ${REPO}/${IMAGE}:latest /VERSIONS | grep readsb | cut -d " " -f 2)
+docker pull "${REPO}/${IMAGE}:latest"
+VERSION=$(docker run --rm --entrypoint cat "${REPO}/${IMAGE}:latest" /VERSIONS | grep readsb | cut -d " " -f 2)
 
 # Build & push version-specific
-docker buildx build -t ${REPO}/${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platform "${PLATFORMS}" .
