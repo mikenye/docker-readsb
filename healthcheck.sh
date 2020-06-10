9577c4d17bb66833a06bf2a7a6384dcf11d145e5
Mike Nye <mike.nye@gmail.com>
2020-06-10 17:18:10 +0800

    Handle a different/missing json path

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,28 +3,36 @@ set -e
 
 EXITCODE=0
 
-# get latest timestamp of readsb json update
-TIMESTAMP_LAST_READSB_UPDATE=$(cat /run/readsb/aircraft.json | jq '.now')
+if [ -f "/run/readsb/aircraft.json" ]; then
 
-# get current timestamp
-TIMESTAMP_NOW=$(date +"%s.%N")
+    # get latest timestamp of readsb json update
+    TIMESTAMP_LAST_READSB_UPDATE=$(cat /run/readsb/aircraft.json | jq '.now')
 
-# makse sure readsb has updated json in past 60 seconds
-TIMEDELTA=$(echo "$TIMESTAMP_NOW - $TIMESTAMP_LAST_READSB_UPDATE" | bc)
-if [ $(echo $TIMEDELTA \< 60 | bc) -ne 1 ]; then
-    echo "readsb last updated: ${TIMESTAMP_LAST_READSB_UPDATE}, now: ${TIMESTAMP_NOW}, delta: ${TIMEDELTA}. UNHEALTHY"
-    EXITCODE=1
+    # get current timestamp
+    TIMESTAMP_NOW=$(date +"%s.%N")
+
+    # makse sure readsb has updated json in past 60 seconds
+    TIMEDELTA=$(echo "$TIMESTAMP_NOW - $TIMESTAMP_LAST_READSB_UPDATE" | bc)
+    if [ $(echo $TIMEDELTA \< 60 | bc) -ne 1 ]; then
+        echo "readsb last updated: ${TIMESTAMP_LAST_READSB_UPDATE}, now: ${TIMESTAMP_NOW}, delta: ${TIMEDELTA}. UNHEALTHY"
+        EXITCODE=1
+    else
+        echo "readsb last updated: ${TIMESTAMP_LAST_READSB_UPDATE}, now: ${TIMESTAMP_NOW}, delta: ${TIMEDELTA}. HEALTHY"
+    fi
+
+    # get number of aircraft
+    NUM_AIRCRAFT=$(cat /run/readsb/aircraft.json | jq '.aircraft | length')
+    if [ $NUM_AIRCRAFT -lt 1 ]; then
+        echo "total aircraft: $NUM_AIRCRAFT. UNHEALTHY"
+        EXITCODE=1
+    else
+        echo "total aircraft: $NUM_AIRCRAFT. HEALTHY"
+    fi
+
 else
-    echo "readsb last updated: ${TIMESTAMP_LAST_READSB_UPDATE}, now: ${TIMESTAMP_NOW}, delta: ${TIMEDELTA}. HEALTHY"
-fi
 
-# get number of aircraft
-NUM_AIRCRAFT=$(cat /run/readsb/aircraft.json | jq '.aircraft | length')
-if [ $NUM_AIRCRAFT -lt 1 ]; then
-    echo "total aircraft: $NUM_AIRCRAFT. UNHEALTHY"
-    EXITCODE=1
-else
-    echo "total aircraft: $NUM_AIRCRAFT. HEALTHY"
+    echo "WARNING: Cannot find /run/readsb/aircraft.json, so skipping some checks."
+
 fi
 
 # death count for lighttpd
